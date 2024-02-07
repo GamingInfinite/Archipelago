8c6fcf5e4e34ac82d1563d418ec6bd6fe936b297
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2024-02-07 13:33:57 -0600

    add Sulfur Springs

--- worlds/bloonstd6/Locations.py
+++ worlds/bloonstd6/Locations.py
@@ -58,6 +58,7 @@ class BloonsLocations:
             "CoveredGarden",
             "Quarry",
             "QuietStreet",
+            "SulfurSprings",
             "BloonariusPrime",
             "Balance",
             "Encrypted",
